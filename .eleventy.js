5108a72ce14c8659301ab52210648c83bd9688b5
quentinmay <quentinmay@users.noreply.github.com>
2021-07-25 01:49:14 +0000

    deploy: 36df16e48c0fadbdde8e375a7172c5df3716d136

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("styles/main.css");
-    eleventyConfig.addPassthroughCopy("styles/grid.css");
-    eleventyConfig.addPassthroughCopy("styles/flex.css");
-};
